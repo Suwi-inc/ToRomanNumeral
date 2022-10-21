0ec7cc715a2c3423c79c2413f0a53599b922aa7d
suwilanji silwamba <57032120+Suwi-inc@users.noreply.github.com>
2022-10-22 01:23:59 +0300

    Add project files.

---
+++ ToRomanNumeral/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ToRomanNumeral
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Value = 0;
+            while (Value != -1)
+            {
+                Console.WriteLine("Enter an Integer Value less than 1000: ");
+                try
+                {
+                    Value = Int32.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                Roman roman = new Roman();
+
+                Console.WriteLine(Value + " As a Roman Numeral = " + roman.CalculateRomanValue(Value));
+            }
+
+
+        }
+    }
+
+      
+}
